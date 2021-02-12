666399150ee615322a277871a366ed41f36a41c3
Romel Pascua <romelpascua14@gmail.com>
2021-02-11 22:14:26 -0800

    create cat function

--- cogs/shibainu.py
+++ cogs/shibainu.py
@@ -23,6 +23,18 @@ class Simba(commands.Cog):
         await ctx.send('**BORKBORK**')
         await ctx.send(shiba[0])
 
+    @commands.command()
+    async def kitty(self, ctx):
+        url = "https://api.thecatapi.com/v1/images/search"
+        header = {"x-api-key": "bab62365-5e13-46c1-8047-a0e9cfaa9299"}
+        response = requests.get(url, header)
+        kitty = response.json() # retrieves dictionary
+        await ctx.send('**MEOWWW**')
+        if len(kitty[0].get("breeds")) != 0:
+            await ctx.send(kitty[0].get("breeds")[0].get("description"))
+        await ctx.send(kitty[0].get("url"))
+
+
 # sets up cog
 def setup(client):
     client.add_cog(Simba(client))
